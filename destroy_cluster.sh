d9d3c8cd1dc317bb109d85e41bbbef7136c26b44
Yolanda Robla <yroblamo@redhat.com>
2019-03-03 07:48:00 +0100

    Do not clean if build does not exist

--- destroy_cluster.sh
+++ destroy_cluster.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 BUILDDIR="$1"
+
+if [ ! -d "${BUILDDIR}" ]; then
+    echo "Cannot destroy cluster, build directory does not exist
+    exit 0
+fi
+
 pushd ${BUILDDIR}
 ./openshift-install destroy cluster
 
